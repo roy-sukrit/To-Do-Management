7f0164ce598876fc93cc4dee57e17b58d50b310d
“roy-sukrit” <roysukrit78@gmail.com>
2024-10-17 20:06:16 +0100

    Added Health Check and Open Api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,16 @@ const PORT = process.env.PORT || 8080;
 //Swagger Init
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+//Global - HealthCheck API
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'UP',
+    message: 'Server is running smoothly!!',
+    responseTime:process.hrtime,
+    timestamp: new Date().toISOString(),
+  });
+});
+
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
