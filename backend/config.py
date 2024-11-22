4061c1bf484bfe5667dea79cbbbda2fa93ff1278
Xinyi729 <835574934@qq.com>
2024-11-22 13:09:43 +0000

    secure

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,13 @@
 import pytest
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
 
 @pytest.fixture
 def base_url():
-    base_url = "http://ec2-98-81-1-246.compute-1.amazonaws.com:8000/api"
+    base_url = os.getenv("BASE_URL")
+    print(base_url)
     return base_url
