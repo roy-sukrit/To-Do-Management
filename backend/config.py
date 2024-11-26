3570ae5a64cbac0bae9505b1b22d786b37a76895
“roy-sukrit” <roysukrit78@gmail.com>
2024-11-26 09:53:51 +0000

    test

--- backend/config.py
+++ backend/config.py
@@ -9,5 +9,5 @@ load_dotenv()
 @pytest.fixture
 def base_url():
     base_url = os.getenv("BASE_URL")
-    print(base_url)
-    return base_url
+    print("base_url",base_url)
+    return 'http://ec2-98-81-1-246.compute-1.amazonaws.com:8000/api'
